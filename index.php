17fa883fa542cb2f3212111150bb4b8e5fcb8b60
adoletskiy <adoletsky@indigos.ru>
2015-03-17 13:58:22 +0300

    first step

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Psyhobbit</title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <link href="css/main.css" rel="stylesheet">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+</head>
+
+<body>
+<div class="text">
+    Когда-то давно старый индеец открыл своему внуку одну жизненную истину:<br>
+    - В каждом человеке идёт борьба, очень похожая на борьбу двух волков.<br>
+    Один волк представляет зло - зависть, ревность, сожаление, эгоизм, амбиции, ложь. Другой волк представляет добро - мир, любовь, надежду, истину, доброту и верность.<br>
+    Маленький индеец, тронутый до глубины души словами деда, на несколько мгновений задумался, а потом спросил:<br>
+    - А какой волк в конце побеждает?<br>
+    Старый индеец едва заметно улыбнулся и ответил:<br>
+    - Всегда побеждает тот волк, которого ты кормишь.
+    <div class="menu-bottom">
+        <ul>
+            <li>Услуги</li>
+            <li>Истории</li>
+            <li>Контакты</li>
+            <li>Обо мне</li>
+        </ul>
+    </div>
+</div>
+    <div class="imgback blur">
+
+    </div>
+</body>
+</html>
