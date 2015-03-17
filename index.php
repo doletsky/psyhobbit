d8c068c9d0fd0363fe2b48ac08548fbd4533aa27
adoletskiy <adoletsky@indigos.ru>
2015-03-17 16:56:55 +0300

    half-maket step

--- index.php
+++ index.php
@@ -11,7 +11,18 @@
 </head>
 
 <body>
+<div class="person">
+    <div class="foto"></div>
+    <div class="name">Александра Долецкая</div>
+    <div class="subtitle">психолог, бизнес-тренер</div>
+    <div class="phone">+7(926)389-01-54</div>
+    <div class="mail">doletskaya@bk.ru</div>
+    <div class="action">
+        <a class="get_consult" href="#">Записаться на консультацию</a>
+    </div>
+</div>
 <div class="text">
+    <div class="title">Истории</div>
     Когда-то давно старый индеец открыл своему внуку одну жизненную истину:<br>
     - В каждом человеке идёт борьба, очень похожая на борьбу двух волков.<br>
     Один волк представляет зло - зависть, ревность, сожаление, эгоизм, амбиции, ложь. Другой волк представляет добро - мир, любовь, надежду, истину, доброту и верность.<br>
@@ -21,10 +32,10 @@
     - Всегда побеждает тот волк, которого ты кормишь.
     <div class="menu-bottom">
         <ul>
-            <li>Услуги</li>
-            <li>Истории</li>
-            <li>Контакты</li>
-            <li>Обо мне</li>
+            <li class="service">Услуги</li>
+            <li class="talk">Истории</li>
+            <li class="contacts">Спросить</li>
+            <li class="about">Обо мне</li>
         </ul>
     </div>
 </div>
