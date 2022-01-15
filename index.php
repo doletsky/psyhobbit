ff4a9a8d4dc4623d7879d17869031230aae942c3
Александр <doletsky@bk.ru>
2022-01-15 21:13:35 +0300

    скрыл разделы и запись

--- index.php
+++ index.php
@@ -59,128 +59,12 @@
     <div class="phone">+7(926)389-01-54</div>
     <div class="mail">doletskaya@bk.ru</div>
     <div class="subtitle i">взрослым, желающим быть ещё счастливее</div>
-    <div class="action">
-        <a class="get_consult" href="#wincons">Записаться на консультацию</a><br>
-        <?php
-        if(0):?><a href="map.html" target="_blank" onClick="popupWin = window.open(this.href, 'contacts', 'location,width=600,height=600,top=0'); popupWin.focus(); return false;" class="load-map">Где проходят консультации</a><?php endif?>
-    </div>
-</div>
-<div class="win_get_consult">
-    <div class="win_get_consult_header">Запись на консультацию<span class="close">x</span> </div>
-    <div class="win_get_consult_body">
-        <input type="hidden" id="GCform" name="formName" value="getCons">
-        <table>
-            <tr>
-                <td>
-                    Имя:
-                </td>
-                <td>
-                    <input id="GCname" type="text" size="50" placeholder="Как к Вам обращаться">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Тел./email:
-                </td>
-                <td>
-                    <input id="GCcontacts" type="text" size="50" placeholder="Удобный для Вас способ связи">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Дата:
-                </td>
-                <td>
-                    <input id="GCdata" type="date" placeholder="Желаемая дата консультации">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Время:
-                </td>
-                <td>
-                    <input id="GCtime" type="time" placeholder="Желаемое время консультации">
-                </td>
-            </tr>
-        </table><br>
-        <u>Тема консультации:</u><br>
-        <textarea id="GCtext" rows="10" cols="68" placeholder="Кратко опишите Вашу тему для консультирования"></textarea><br>
-        <a href="#getcons" class="get_consult_yet">Записаться</a>
-    </div>
-    <div class="win_get_consult_after">Ваш запрос отправлен.<br>Я свяжусь с Вами по указанным контактам в течении дня.</div>
-    <div class="win_get_consult_footer">
-        * Стоимость одного часа индивидуальной работы 2500 руб.
-        Данная форма является запросом на консультацию. Дата и время подтверждаются личным контактом по телефону.
-    </div>
-</div>
-<div class="text">
-
-    <div class="title">Миссия</div>
-    <div class="text-content" id="miss">
-        <?php
-        echo $miss=str_replace("\n", "<br />",file_get_contents('texts/missiya.txt'));
-        ?>
-    </div>
-    <div class="text-content" id="service">
-        <?php
-        echo $miss=file_get_contents('texts/service.txt');
-        ?>
-    </div>
-
-        <?php
-        //history box
-        $dir='texts/history/';
-        $listFile=scandir($dir);
-        unset($listFile[0]);
-        unset($listFile[1]);
-
-        $strPoint='';
-        $strHTML='<div class="text-content" id="talk">';
-        foreach($listFile as $fname){
-            $idName=trim($fname,".txt");
-            $strPoint.='<div class="point" id="'.$idName.'"></div>';
-            $miss=str_replace("\n", "<br />",file_get_contents($dir.$fname));
-            $strHTML.='<div class="slide"  id="'.$idName.'">'.$miss.'</div>';
-        }
-        $strHTML.='</div><div class="slide-point" id="talk"><div class="button-next">></div><div class="view-time"><div class="load-time"></div></div>'.$strPoint.'</div>';
-        echo $strHTML;
-        ?>
-
-    <div class="text-content" id="contacts">
-    <form>
-        <textarea id="askText" cols="70" rows="12" placeholder="Здесь напишите Ваш вопрос."></textarea>
-        <div class="askme">
-            <input type="hidden" id="askForm" name="askForm" value="getAsk">
-            Обязательно укажите, как к Вам обращаться и куда ответить<br><br>
-            Ваше имя: <input id="askName" type="text" size="58" placeholder="Как я могу к Вам обращаться?"><br>
-            Телефон: <input id="askTel" type="text" size="58" placeholder="Если укажете телефон, то я Вам позвоню."><br>
-            E-mail: <input id="askEmail" type="email" size="58" placeholder="Если укажете e-mail, то я смогу ответить письмом."><br>
-            <input id="askButton" type="button" value="Спросить">
-        </div>
-
-    </form>
-    </div>
-    <div class="text-content" id="about">
-        <?php
-        echo $miss=str_replace("\n", "<br />",file_get_contents('texts/about_me.txt'));
-        ?>
-    </div>
-    <div class="menu-bottom">
-        <ul>
-            <li class="miss">Миссия</li>
-            <li class="service">Услуги</li>
-            <li class="about">Обо мне</li>
-            <li class="talk">Истории</li>
-            <li class="contacts">Спросить</li>
-
-        </ul>
-    </div>
 </div>
     <div class="imgback blur">
 
     </div>
 <div class="copyright">
-    &copy; Александра Долецкая. 2015г.
+    &copy; Александра Долецкая. 2015г.-2022г.
 </div>
 </body>
 </html>
